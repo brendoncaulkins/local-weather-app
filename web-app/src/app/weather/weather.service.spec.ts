6b089a76c95b3c7cab5b7d5630275cf297ead05b
brendoncaulkins <brendon.caulkins@excella.com>
2018-03-03 20:58:14 -0500

    Finishes basic Angular Material styling, including updated test imports

--- web-app/src/app/weather/weather.service.spec.ts
+++ web-app/src/app/weather/weather.service.spec.ts
@@ -7,8 +7,7 @@ describe('WeatherService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [WeatherService],
-      imports: [HttpClientTestingModule],
+      providers: [WeatherService]
     })
   })
 
